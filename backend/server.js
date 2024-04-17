b0aa894b9cf2e2aff369dbcca9ef702b41d33bfd
Sami Moqbel <moqbelsami@gmail.com>
2024-04-17 08:33:19 +0300

    Deployment

--- backend/server.js
+++ backend/server.js
@@ -6,11 +6,14 @@ const usersRoutes = require("./routes/users.routes");
 const cookieParser = require("cookie-parser");
 const connectToMongoDB = require("./db/connectToMongoDB");
 const { app, server } = require("./socket/socket");
+const path = require("path");
 
 
+const PORT= process.env.PORT || 5000;
+
+// const __dirname = path.resolve();
 
 dotenv.config();
-const PORT= process.env.PORT || 5000;
 
 
 app.use(express.json()); // req.body
@@ -19,6 +22,13 @@ app.use(cookieParser());
 app.use("/api/auth", authRoutes);
 app.use("/api/messages", messageRoutes);
 app.use("/api/users", usersRoutes);
+
+// console.log("dirname", __dirname);
+app.use(express.static(path.join(__dirname, "../frontend/build")));
+
+app.get("*", (req, res) => {
+    res.sendFile(path.join(__dirname, "../frontend/build/index.html"));
+});
 
 server.listen(PORT, () => { 
     connectToMongoDB();
