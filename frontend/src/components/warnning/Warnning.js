96c635548773fa2a45da6cac7d9e55ba49b43bab
Sami Moqbel <moqbelsami@gmail.com>
2024-04-17 14:24:42 +0300

    warning added

---
+++ frontend/src/components/warnning/Warnning.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const Warnning = () => {
+  return (
+    <div className='border-r border-slate-500 p-4 flex flex-col justify-center items-center backdrop-filter backdrop-blur-lg bg-opacity-0  '>
+      <h1 class="text-2xl font-bold text-blue-100">Warnning !!</h1>
+        <p class="text-lg text-white max-w-[10rem] break-words">These people are Real not Fake Please watchout ! Thanks {'<3'} </p>
+        <p class="text-m text-green-500">By : Sami Moqbel </p>
+    </div>
+  )
+}
+
+export default Warnning
